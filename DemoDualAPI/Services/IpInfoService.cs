cd4af6b8382eb0c723f7c8a56cff6efcc2b5dca5
Fady <145761517+Manhattaa@users.noreply.github.com>
2024-01-09 14:48:15 +0100

    Add project files.

---
+++ DemoDualAPI/Services/IpInfoService.cs
@@ -0,0 +1,20 @@
+﻿namespace DemoDualAPI.Services
+{
+    public class IpInfoService
+    {
+        private HttpClient _client;
+        public IpInfoService(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public IpInfoService() : this(new HttpClient())
+        {
+
+        }
+        public async Task<string> GetCityAsync()
+        {
+
+        }
+    }
+}
